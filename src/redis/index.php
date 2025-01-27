047cb11ac5091ffd85447baaa9d60830f941755a
t.lytvyn <t.lytvyn@reactor.com.ua>
2025-01-27 17:33:12 +0200

    add base redis index files

--- src/redis/index.php
+++ src/redis/index.php
@@ -2,6 +2,30 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$redis = new \Redis();
-$redis->connect('pmc_redis', 6379);
-echo $redis->ping();
+try {
+    $redis = new Predis\Client([
+        'scheme' => 'tcp',
+        'host'   => 'pmc_redis',
+        'port'   => 6379,
+    ]);
+    $key = 'test_key';
+    $ttl = 10;
+    $check = $redis->exists($key);
+
+    if ($check) {
+        $ttl = $redis->ttl($key);
+        echo " [*] TTL: $ttl \n";
+        if ($ttl > 0) {
+            echo " [*] Exists value: " . $redis->get($key) . "\n";
+        } else {
+            echo " [*] Delete: $key \n";
+            $redis->del($key);
+        }
+    } else {
+        echo " [*] Set: $key \n";
+        $redis->set($key, "test value");
+        $redis->expire($key, $ttl);
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
